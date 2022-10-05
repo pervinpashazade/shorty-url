e501401d67453a3549bf704a23844169f7b63390
parvin.pashazada <parvin.pashazada@a2z.az>
2022-10-05 13:37:48 +0400

    sequelize setup

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
+import db from "./src/DAL/models/index.cjs"
 
+// const { db } = require("./src/DAL/models/index.cjs");
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,10 @@ app.use(bodyParser.json());
 app.use(cors({
     origin: "*"
 }));
+
+await db.sequelize.sync({ alter: true }).then(() => {
+    console.log("sequelize resync database")
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
