f3682a82f3e738162c190e695458e60985780fa9
parvin.pashazada <parvin.pashazada@a2z.az>
2022-10-08 06:55:06 +0400

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './assets/bootstrap/bootstrap.scss';
 import './assets/scss/style.scss';
 import {
@@ -8,41 +9,49 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
 import { config } from './config';
+import logo from "./assets/img/logo.png"
 
 const App = () => {
+
+  const [switchStatus, setSwitchStatus] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({
+    url: [],
+  });
 
   const handleSubmit = e => {
     e.preventDefault();
 
     const form = new FormData(e.target);
     let formData = {};
+    let errors = {
+      url: [],
+    }
 
     for (const [key, value] of form.entries()) {
       formData[key] = value.trim();
     };
 
     if (!formData.url) {
-      toast.error('Opps, URL is required!', {
-        position: "bottom-right",
-        autoClose: 5000,
-        newestOnTop: true,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        pauseOnFocusLoss: true,
-        theme: 'dark',
-      });
-      return
+      toast.error('Opps, URL is required!');
+      errors.url = [...errors.url, "This field is required"]
     }
+
+    setValidationErrors(errors);
+
+    if (errors.url.length) return;
 
     axios.post(config.apiURL + 'api/v1/shortlinks', {
       url: formData.url,
-      provider: "bit.ly"
+      provider: !switchStatus ? "bit.ly" : "TinyURL"
     }).then(res => {
       console.log('res', res);
-    })
+    }).catch(err => {
+      if (err.response?.status === 422) {
+        setValidationErrors({
+          url: err.response.data.error.url ? err.response.data.error.username : [],
+        });
+      }
+    });
 
   }
 
@@ -52,10 +61,9 @@ const App = () => {
         <div className="row justify-content-center">
           <div className="col-md-12 col-lg-10">
             <div className="wrapper">
-              {/* p-md-4 p-lg-5 */}
               <div className="box form-wrapper">
-                {/* <ToastContainer
-                  position="top-right"
+                <ToastContainer
+                  position="bottom-right"
                   autoClose={5000}
                   hideProgressBar={false}
                   newestOnTop={false}
@@ -65,33 +73,57 @@ const App = () => {
                   draggable
                   pauseOnHover
                   theme='dark'
-                /> */}
-                <ToastContainer />
+                />
                 <Form
                   className='row'
                   onSubmit={e => handleSubmit(e)}
                 >
                   <div className="col-12 mb-4">
-                    <Label for='url'>URL</Label>
+                    <Label for='url'>
+                      URL
+                      {
+                        validationErrors.url.length > 0 &&
+                        <span className='text-danger ml-1'>*</span>
+                      }
+                    </Label>
                     <Input
                       id='url'
                       name='url'
-                      placeholder='Enter URL to shorten'
+                      onChange={() => {
+                        if (validationErrors.url.length) {
+                          setValidationErrors(prevState => {
+                            return {
+                              ...prevState,
+                              url: []
+                            }
+                          })
+                        }
+                      }}
+                      placeholder='https://example.com/JohnDoe'
                     />
+                    {
+                      validationErrors.url.length > 0 &&
+                      <div className='validation-errors'>
+                        {
+                          validationErrors.url.map((item, index) => <span key={index}>{item}</span>)
+                        }
+                      </div>
+                    }
                   </div>
                   <div className="col-12 mb-4">
                     <div className="d-flex align-items-center">
-                      <span className='font-weight-bold'>Bit.ly</span>
+                      <span className={`${!switchStatus ? 'font-weight-bold' : ''}`}>Bit.ly</span>
                       <div className='d-flex mx-4'>
                         <input
-                          type="checkbox"
-                          name="toggle1"
-                          className="ios-switch-btn"
                           id="toggle1"
+                          name="switch"
+                          type="checkbox"
+                          className="ios-switch-btn"
+                          onChange={e => setSwitchStatus(e.target.checked)}
                         />
                         <label htmlFor="toggle1"></label>
                       </div>
-                      <span>TinyURL</span>
+                      <span className={`${switchStatus ? 'font-weight-bold' : ''}`}>TinyURL</span>
                     </div>
                   </div>
                   <div className="col-12 mb-2">
@@ -105,7 +137,6 @@ const App = () => {
                   </div>
                 </Form>
               </div>
-              {/* p-md-4 p-lg-5 */}
               <div className="box detail-wrapper">
                 <h1>Welcome</h1>
                 <p>to <strong>Shortly URL</strong> shortener!</p>
--- client/src/interceptor.js
+++ client/src/interceptor.js
@@ -1,17 +1,17 @@
 import React from "react";
 import axios from 'axios';
-import { localStorageKeys } from "./config123";
+// import { localStorageKeys } from "./config123";
 import { store } from './store/store';
 import { logout } from './store/action';
-import toast from 'toasted-notes';
+import { toast } from 'react-toastify';
 import { message, renderErrorMessage } from './store/staticData';
 
 axios.interceptors.request.use(
   config => {
-    const token = localStorage.getItem(localStorageKeys.token);
-    if (token && config.url.indexOf('auth') === -1) {
-      config.headers['Authorization'] = 'Bearer ' + token;
-    }
+    // const token = localStorage.getItem(localStorageKeys.token);
+    // if (token && config.url.indexOf('auth') === -1) {
+    //   config.headers['Authorization'] = 'Bearer ' + token;
+    // }
     return config;
   }, error => {
     Promise.reject(error);
@@ -23,66 +23,57 @@ axios.interceptors.response.use(
     return response;
   }, error => {
     if (!error?.response) {
-      toast.notify(({ onClose }) => (
-        <div className="alert alert-danger m-3">
-          <h5>Sistem xətası!</h5>
-          <p className="mb-0">
-            Serverlə əlaqə qurula bilmədi.
-          </p>
-        </div>), { position: "top-right", duration: 3000 });
+
+      toast.error("ERROR")
 
       return Promise.reject(error);
     }
     else if (error.response.status === 400) {
-      toast.notify(({ onClose }) => (
-        <div className="alert alert-danger m-3">
-          <h5>Xəta baş verdi!</h5>
-          <p className="mb-0">
-            {
-              error.response.data.message ? error.response.data.message : '.'
-            }
-          </p>
-        </div>), { position: "top-right", duration: 3000 });
-      return Promise.reject(error)
-    }
-    else if (error.response.status === 409) {
-      toast.notify(({ onClose }) => (
-        <div className="alert alert-danger m-3">
-          <h5>Xəta baş verdi!</h5>
-          <p className="mb-0">
-            {
-              error.response.data.message ? error.response.data.message : '.'
-            }
-          </p>
-        </div>), { position: "top-right", duration: 3000 });
+
+      console.log('error.response.data', error.response.data);
+
+      // toast.notify(({ onClose }) => (
+      //   <div className="alert alert-danger m-3">
+      //     <h5>Xəta baş verdi!</h5>
+      //     <p className="mb-0">
+      //       {
+      //         error.response.data.message ? error.response.data.message : '.'
+      //       }
+      //     </p>
+      //   </div>), { position: "top-right", duration: 3000 });
+
+      toast.error("ERROR")
+
       return Promise.reject(error)
     }
     else if (error.response.status === 422) {
-      toast.notify(({ onClose }) => (
-        <div className="alert alert-danger m-3">
-          <h5>Xəta baş verdi!</h5>
-          {/* <p className="mb-0">
-            {
-              error.response.data.message ? error.response.data.message : '.'
-            }
-          </p> */}
-        </div>), { position: "top-right", duration: 3000 });
+
+      console.log('test', error.response.data.error.url[0]);
+
+      toast.error(
+        <div>
+          {error.response.data.error.url[0]}
+        </div>
+      )
+
       return Promise.reject(error)
     }
 
     return new Promise((resolve) => {
 
       if (error.response.status === 401) {
-        toast.notify(() => (
-          <div className="alert alert-danger m-3">
-            <h5>{message.error}</h5>
-            <p className="mb-0">
-              {renderErrorMessage(message.user_not_found)}
-            </p>
-          </div>), { position: "top-right", duration: message.duration }
-        );
-        const { dispatch } = store;
-        dispatch(logout());
+        // toast.notify(() => (
+        //   <div className="alert alert-danger m-3">
+        //     <h5>{message.error}</h5>
+        //     <p className="mb-0">
+        //       {renderErrorMessage(message.user_not_found)}
+        //     </p>
+        //   </div>), { position: "top-right", duration: message.duration }
+        // );
+        // const { dispatch } = store;
+        // dispatch(logout());
+        toast.error("ERROR")
+
       }
 
       // // if(condition) resolve(response);
